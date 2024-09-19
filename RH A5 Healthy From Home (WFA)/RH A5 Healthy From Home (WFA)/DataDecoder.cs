de9f9f2264cdf5070d41917dbb099dae885abbe4
DuckingMCDuck <jr.klein@student.avans.nl>
2024-09-19 16:52:01 +0200

    Committed unfinished relocation of project to a windows forum application.

---
+++ RH A5 Healthy From Home (WFA)/RH A5 Healthy From Home (WFA)/DataDecoder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace RH_A5_Healthy_From_Home
+{
+    public class DataDecode
+    {
+
+        public static List<(string, int)> listForToString = new List<(string, int)>();
+
+        public static List<(string, int)> Decode(string data)
+        {
+
+
+            //try
+            //{
+                List<(string, int)> dataWithNames = new List<(string, int)>();
+                List<int> difDataInt = new List<int>();
+                string[] substrings1 = data.Split(':', ' ');
+                //string[] substrings2 = data.Split(',', ' ');
+                string chopedData = substrings1[1];
+
+                string[] splitData = chopedData.Split(' ');
+                for (int i = 0; i < splitData.Length; i++)
+                {
+                int decValue = Convert.ToInt32(splitData[i], 16);
+                difDataInt.Add(decValue);
+                }
+                if (difDataInt[4] == 16)
+                {
+                    for (int i = 0; i < difDataInt.Count; i++)
+                    {
+                        string name = Enum.GetName(typeof(DataNames16), i);
+                        (string, int) tuple = (name, difDataInt[i]);
+                        dataWithNames.Add(tuple);
+                    }
+
+                }
+                if (difDataInt[4] == 25)
+                {
+                    for (int i = 0; i < difDataInt.Count; i++)
+                    {
+                        string name = Enum.GetName(typeof(DataNames25), i);
+                        (string, int) tuple = (name, difDataInt[i]);
+                        dataWithNames.Add(tuple);
+                    }
+                }
+
+                
+                listForToString = dataWithNames;
+                Console.WriteLine(MakeString(dataWithNames));
+                return dataWithNames;
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Not able to decode data");
+            //    return null;
+            //}
+        }
+
+        public static string MakeString(List<(string, int)> list)
+        {
+            try
+            {
+                string result = String.Empty;
+            
+            
+            for (int i = 0; i < listForToString.Count; i++)
+                {
+                    result += $"{listForToString[i].Item1}: {listForToString[i].Item2} \n";
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                return "Not able to make string";
+            }
+        }
+    }
+
+}
---
+++ RH A5 Healthy From Home (WFA)/RH A5 Healthy From Home (WFA)/DataNames.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RH_A5_Healthy_From_Home
+{
+    enum DataNames16
+    {
+        Sync,
+        Msg_Lenght,
+        Msg_ID,
+        Channel_ID,
+        Data_Page_Number,
+        Equipment_Type_Bit_Field,
+        Elapsed_Time,
+        Distance_Traveled,
+        Speed_LSB,
+        Speed_MSB,
+        HeartRate,
+        Capabilities_Bit_Field,
+        FE_State_Bit_Field,
+        Checksum
+    }
+
+    enum DataNames25
+    {
+        Sync,
+        Msg_Lenght,
+        Msg_ID,
+        Channel_ID,
+        Data_Page_Number,
+        Update_Event_Count,
+        Instantaneous_Cadence,
+        Accumulated_Power_LSB,
+        Accumulated_Power_MSB,
+        Instantaneous_Power_LSB,
+        Instantaneous_Power_MSB_and_Trainer_Status_Bit_Field,
+        Flags_Bit_Field,
+        FE_State_Bit_Field,
+        Checksum
+
+    }
+
+    enum HeartRate
+    {
+        HeartRate
+    }
+}
