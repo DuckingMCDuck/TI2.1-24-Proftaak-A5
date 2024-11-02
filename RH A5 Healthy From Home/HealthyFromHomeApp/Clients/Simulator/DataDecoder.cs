fac2f03960030fd7b69636639d966c8189b5e3c2
DuckingMCDuck <jr.klein@student.avans.nl>
2024-11-02 10:22:56 +0100

    Quick change to datadecoder

--- RH A5 Healthy From Home/HealthyFromHomeApp/Clients/Simulator/DataDecoder.cs
+++ RH A5 Healthy From Home/HealthyFromHomeApp/Clients/Simulator/DataDecoder.cs
@@ -164,39 +164,112 @@ namespace HealthyFromHomeApp.Clients
 
                 if (difDataInt[4] == 16)
                 {
-                    int lsbValue = 0;
-                    int msbValue = 0;
+                    int speedLsbValue = 0;
+                    int speedMsbValue = 0;
+
                     Boolean calcSpeed = false;
                     for (int i = 0; i < difDataInt.Count; i++)
                     {
                         string name = Enum.GetName(typeof(DataNames16), i);
                         if (name == "Speed_LSB")
                         {
-                            lsbValue = difDataInt[i];
+                            speedLsbValue = difDataInt[i];
                         }
-                        else if (name == "Speed_MSB" && lsbValue != 0)
+                        if (name == "Speed_MSB")
                         {
-                            msbValue = difDataInt[i];
+                            speedMsbValue = difDataInt[i];
                             calcSpeed = true;
                         }
                         (string, int) tuple = (name, difDataInt[i]);
                         dataWithNames.Add(tuple);
                         if (calcSpeed == true)
                         {
-                            double speed = ((msbValue << 8) | lsbValue) / 1000 * 3.6;
+                            double speed = ((speedMsbValue << 8) | speedLsbValue) / 1000 * 3.6;
                             (string, int) speedTuple = ("Speed", (int)Math.Round(speed));
                             dataWithNames.Add(speedTuple);
                             calcSpeed = false;
                         }
                     }
                 }
-                else if (difDataInt.Count > 4 && difDataInt[4] == 25)
+                else if (difDataInt[4] == 25)
                 {
+                    int accumulatedLsbValue = 0;
+                    int accumulatedMsbValue = 0;
+
+                    int instantaneousLsbValue = 0;
+                    int instantaneousValue = 0;
+
+                    Boolean calcAccumulatedPower = false;
+                    Boolean calcInstantaneousPower = false;
+
                     for (int i = 0; i < difDataInt.Count; i++)
                     {
                         string name = Enum.GetName(typeof(DataNames25), i);
-                        (string, int) tuple = (name, difDataInt[i]);
-                        dataWithNames.Add(tuple);
+
+                        if (name == "Accumulated_Power_LSB")
+                        {
+                            accumulatedLsbValue = difDataInt[i];
+                        }
+                        if (name == "Accumulated_Power_MSB")
+                        {
+                            accumulatedMsbValue = difDataInt[i];
+                            calcAccumulatedPower = true;
+                        }
+
+
+                        if (name == "Instantaneous_Power_LSB")
+                        {
+                            instantaneousLsbValue = difDataInt[i];
+                        }
+
+                        if (name == "Instantaneous_Power_MSB_and_Trainer_Status_Bit_Field")
+                        {
+                            string binaryString = Convert.ToString(difDataInt[i], 2);
+                            int lengthMissing = 8 - binaryString.Length;
+                            for (int x = 0; x < lengthMissing; x++)
+                            {
+                                binaryString = "0" + binaryString;
+                            }
+                            string powerPart = binaryString.Substring(4, 4);
+                            instantaneousValue = Convert.ToInt32(powerPart, 2);
+                            string bitField = binaryString.Substring(0, 4);
+                            int bitFieldInt = Convert.ToInt32(bitField, 2);
+
+
+                            (string, int) tuplePower = ("Instantaneous_Power", instantaneousValue);
+                            dataWithNames.Add(tuplePower);
+                            (string, int) tupleBitField = ("Trainer_Status_Bit_Field", bitFieldInt);
+                            dataWithNames.Add(tupleBitField);
+                            calcInstantaneousPower = true;
+
+
+                        }
+                        else
+                        {
+                            (string, int) tuple = (name, difDataInt[i]);
+                            dataWithNames.Add(tuple);
+                        }
+
+
+                        if (calcAccumulatedPower)
+                        {
+                            double accumulatedPower = ((accumulatedMsbValue << 8) | accumulatedLsbValue);
+                            (string, int) accumulatedPowerTuple = ("Accumulated_Power", (int)Math.Round(accumulatedPower));
+
+                            dataWithNames.Add(accumulatedPowerTuple);
+
+                            calcAccumulatedPower = false;
+                        }
+
+
+                        if (calcInstantaneousPower)
+                        {
+                            double instantaneousPower = ((instantaneousValue << 8) | instantaneousLsbValue);
+                            (string, int) instantaneousPowerTuple = ("Instantaneous_Power", (int)Math.Round(instantaneousPower));
+                            dataWithNames.Add(instantaneousPowerTuple);
+                            calcInstantaneousPower = false;
+                        }
+
                     }
                 }
                 else if (difDataInt.Count >= 1 && difDataInt.Count <= 10)
@@ -205,7 +278,6 @@ namespace HealthyFromHomeApp.Clients
                     (string, int) tuple = (name, difDataInt[1]);
                     dataWithNames.Add(tuple);
                 }
-
                 listForToString = dataWithNames;
 
                 if (clientInstance != null)
